61be675eab7f36e3393691acfc3dc2d6462c49a8
Qaioos <legend442005@gmail.com>
2026-03-13 14:43:04 +0300

    feat: implement TopHeader component and integrate react-icons

--- src/Components/header/TopHeader.jsx
+++ src/Components/header/TopHeader.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import logo from "../../img/logo.png";
+
+// Css Module
+import './header.css'
+//React Router
+import { Link } from "react-router-dom";
+
+//React Icons
+import { FaSearch } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa6";
+import { TiShoppingCart } from "react-icons/ti";
+
 
 export default function TopHeader() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="top_header">
+            <div className="contaier">
+                <Link to="/" className="logo">
+                    <img src={logo} alt="logo" />
+                </Link>
+
+                <form action="" className="search_box">
+                    <input
+                        type="text"
+                        name="search"
+                        id="search"
+                        placeholder="search for products"
+                    />
+                    <button type="submit">
+                        <FaSearch />
+                    </button>
+                </form>
+
+                <div className="header_icons">
+                    <div className="icon">
+                        <FaRegHeart />
+                        <span className="count">0</span>
+                    </div>
+
+                    <div className="icon">
+                        <TiShoppingCart />
+                        <span className="count">0</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
+import React from 'react'
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+/* import { createRoot } from 'react-dom/client' */
 import './index.css'
+import  ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter basename='/'>
     <App />
+    </BrowserRouter> 
   </StrictMode>,
 )
