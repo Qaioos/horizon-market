5f2200b2bfa6e77eb0ef0e6607bba52351db80ca
Qaioos <legend442005@gmail.com>
2026-03-22 16:45:19 +0300

    feat: Provider Context Favorites

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { StrictMode } from 'react'
+import React from "react";
+import { StrictMode } from "react";
 /* import { createRoot } from 'react-dom/client' */
-import './index.css'
-import  ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import CartProvider from './Components/Contexts/CartContext.jsx'
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import CartProvider from "./Components/Contexts/CartContext.jsx";
+import  CreateFavoretProvider  from "./Components/Contexts/FavoretContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter basename='/'>
-    <CartProvider>
-      <App />
-    </CartProvider>
-    </BrowserRouter> 
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter basename="/">
+            <CreateFavoretProvider>
+                <CartProvider>
+                    <App />
+                </CartProvider>
+            </CreateFavoretProvider>
+        </BrowserRouter>
+    </StrictMode>,
+);
